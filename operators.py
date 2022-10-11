38a21abac6ecf1ffad1944d39a0a2c0a896035e7
MoeShaa123 <mohamedyusuf01@hotmail.com>
2022-10-11 11:33:00 +0100

    new markdown steps added with github commands

---
+++ operators.py
@@ -0,0 +1,23 @@
+
+# Intro to Data types and operators
+# - + * /
+
+
+# Comparison operators
+# - `>` greater than
+# - `<` less than
+# `==` True or False
+
+# To commit multiple line `ctrl+/`
+
+
+a = 24
+b = 16
+user_age = 18
+
+print(a + b) # operator adds a to b
+print(a - b) # operator minus b from a
+#comparison
+print(a>b) # True
+print(a<b) # False
+print(a==b) # False
