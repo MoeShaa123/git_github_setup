40f8696e0841c21db67710d3ee9a2090f511eaaa
MoeShaa123 <mohamedyusuf01@hotmail.com>
2022-10-11 14:32:17 +0100

    new markdown steps added with github commands

--- operators.py
+++ operators.py
@@ -63,27 +63,6 @@ print(greeting[-6:]) # prints World!
 # print(Example_text.capitalize()) # capitalizes the first letter
 # print(Example_text.replace("with", "'")) # replaces characters in string
 
-# user data input
-first_name = "mohamed"
-last_name = "yusuf"
-salary =  40
-
-print(first_name + " " + last_name + " " + str(salary)) # cast int to string using string()
-
-# F-string
-print(f"Hello {first_name} {last_name}")  # Python 3.5/6 or above
-
-print("What is your name?")
-name = input()
-
-print("What is your DOB")
-DOB = input()
-
-print("Course Name?")
-Course_name = input()
-
-print("What is your door number?")
-Door_number = str(input())
 
 
 print("What is your adress?")
